5d64dbb9ea953441c62858d985852e96850ba1dc
Sharon Yang <sharonyyang0@gmail.com>
2025-10-15 15:25:01 -0400

    add routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from api.routes.users import router as users_router
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+app.include_router(users_router)
