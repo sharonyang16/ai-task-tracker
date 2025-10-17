cf2a22a2ac859f79807c81491eaa8f411816be78
Sharon Yang <92332953+sharonyang16@users.noreply.github.com>
2025-10-17 14:02:51 -0400

    Add Navigation Skeleton (#2)
    * setup basic navigation skeleton
    
    * change to false

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -2,5 +2,15 @@ import React from "react";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  const isLoggedIn = false;
+  return (
+    <Stack>
+      <Stack.Protected guard={isLoggedIn}>
+        <Stack.Screen name="(authenticated)" options={{ headerShown: false }} />
+      </Stack.Protected>
+      <Stack.Protected guard={!isLoggedIn}>
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+      </Stack.Protected>
+    </Stack>
+  );
 }
