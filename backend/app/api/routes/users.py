597fe9ad071ed933b01947b1591898b6b240bd2f
Sharon Yang <92332953+sharonyang16@users.noreply.github.com>
2025-10-21 12:28:09 -0400

    Move Supabase Auth to Backend (#11)
    * create endpoints
    
    * remove unused endpoints
    
    * remove unused import

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -1,16 +1,47 @@
-from typing import Annotated
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Request
+from fastapi.responses import JSONResponse
+from ..services.users import supabase_logout, supabase_signup, supabase_login
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
-@router.get("/users/")
-def get_users():
-    return [{"username": "john"}, {"username": "todd"}]
+@router.post("/signup")
+async def signup(username: str, password: str):
+    try:
+        result = supabase_signup(username, password)
+        return result
+    except Exception as e:
+        return JSONResponse(
+            status_code=400,
+            content={"message": str(e)},
+        )
 
 
-@router.get("/user/{username}/")
-async def read_path(
-    username: Annotated[str, Path(title="The username of the user to get.")],
-):
-    return {"username": username}
+@router.post("/login")
+async def login(username: str, password: str):
+    try:
+        result = supabase_login(username, password)
+        return result
+    except Exception as e:
+        return JSONResponse(
+            status_code=400,
+            content={"message": str(e)},
+        )
+
+
+@router.post("/logout")
+async def login(request: Request):
+    try:
+        bearer_token = request.headers.get("Authorization")
+        if not bearer_token:
+            return JSONResponse(
+                status_code=401,
+                content={"message": "Missing Authorization token"},
+            )
+        result = supabase_logout(bearer_token)
+        return result
+    except Exception as e:
+        return JSONResponse(
+            status_code=400,
+            content={"message": str(e)},
+        )
