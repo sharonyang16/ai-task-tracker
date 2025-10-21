201e10287a95f0b70c7444246b58f6d2e3d4122b
Sharon Yang <sharonyyang0@gmail.com>
2025-10-21 10:07:20 -0400

    update setState call

--- frontend/app/navigator.tsx
+++ frontend/app/navigator.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { Stack } from "expo-router";
 import useSession from "@/hooks/useSession";
-import { useAuth } from "@/context/auth-context";
 
 export default function Navigator() {
   const { session } = useSession();
-  const { setUser } = useAuth();
 
   const isLoggedIn = session !== null;
-
-  if (isLoggedIn) {
-    setUser(session.user);
-  }
 
   return (
     <Stack>
