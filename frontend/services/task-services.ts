725422cfca4e96c5ad615d36ae745a866252dc97
Sharon Yang <92332953+sharonyang16@users.noreply.github.com>
2025-10-21 11:25:13 -0400

    Log out + User Context Restructuring (#10)

--- frontend/services/task-services.ts
+++ frontend/services/task-services.ts
@@ -4,11 +4,7 @@ import { DatabaseTask } from "@/types/tasks";
 const TASKS_API_URL = `${process.env.EXPO_PUBLIC_SERVER_URL}/tasks`;
 
 const getTasksByUUID = async (uuid: string): Promise<DatabaseTask[]> => {
-  console.log(`${TASKS_API_URL}/tasks/${uuid}`);
-
-  const res = await api.get(`${TASKS_API_URL}/tasks/${uuid}`, {
-    validateStatus: () => true,
-  });
+  const res = await api.get(`${TASKS_API_URL}/tasks/${uuid}`);
 
   if (res.status !== 200) {
     throw new Error("Error when fetching users");
