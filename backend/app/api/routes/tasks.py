af5ab81d64c1df80f52bfa2875203412d0df95e6
Sharon Yang <92332953+sharonyang16@users.noreply.github.com>
2025-10-22 10:13:37 -0400

    Add create-task endpoint (#16)
    * black changes
    
    * added endpoint

--- backend/app/api/routes/tasks.py
+++ backend/app/api/routes/tasks.py
@@ -1,8 +1,24 @@
-from typing import Annotated
-from ..services.tasks import get_tasks
+from typing import Annotated, Optional
+from ..services.tasks import get_tasks, create_new_task
 from fastapi import APIRouter, Path
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from enum import Enum
 
 router = APIRouter(prefix="/tasks", tags=["tasks"])
+
+
+class TaskSize(str, Enum):
+    SMALL = "SMALL"
+    MEDIUM = "MEDIUM"
+    LARGE = "LARGE"
+
+
+class CreateTaskRequestBody(BaseModel):
+    title: str
+    description: Optional[str] = None
+    creator: str
+    size: TaskSize
 
 
 @router.get("/tasks/{uuid}/", status_code=200)
@@ -12,3 +28,24 @@ async def read_path(
     ],
 ):
     return get_tasks(uuid)
+
+
+@router.post("/create-task/")
+async def create_task(body: CreateTaskRequestBody):
+    try:
+        title = body.title
+        description = body.description
+        creator = body.creator
+        size = body.size
+        if not title or not creator or not size:
+            return JSONResponse(
+                status_code=400,
+                content={"message": "title, creator, and size are required"},
+            )
+
+        return create_new_task(title, description, creator, size)
+    except Exception as e:
+        return JSONResponse(
+            status_code=400,
+            content={"message": str(e)},
+        )
--- backend/app/api/services/tasks.py
+++ backend/app/api/services/tasks.py
@@ -1,4 +1,5 @@
 from .supabase import supabase
+from datetime import date
 
 
 def get_tasks(uuid):
@@ -11,5 +12,25 @@ def get_tasks(uuid):
             .eq("parent_task_id", task["id"])
             .execute()
         ).data
+
+    return response.data
+
+
+def create_new_task(title, description, creator, size):
+    response = (
+        supabase.table("tasks")
+        .insert(
+            {
+                "created_at": date.today().strftime("%Y-%m-%d"),
+                "title": title,
+                "description": description,
+                "created_by": creator,
+                "sub_tasks": [],
+                "is_complete": False,
+                "size": size,
+            }
+        )
+        .execute()
+    )
 
     return response.data
