90fb3cd36a1018b2cbef28f2e3a3cd487acf2217
Sharon Yang <sharonyyang0@gmail.com>
2025-10-23 13:19:14 -0400

    remove recommendations route

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
 from api.routes.users import router as users_router
-from api.routes.recommendations import router as recommendations_router
 from api.routes.tasks import router as tasks_router
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
