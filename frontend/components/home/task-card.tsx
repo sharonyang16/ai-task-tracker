98a80b7b1fb969a9c23217352af95a1667a1339a
Sharon Yang <sharonyyang0@gmail.com>
2025-10-23 16:37:09 -0400

    show subtasks

--- frontend/components/home/task-card.tsx
+++ frontend/components/home/task-card.tsx
@@ -1,18 +1,30 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-import { DatabaseTask } from "@/types/tasks";
+import { Task } from "@/types/tasks";
 
 type TaskCardProps = {
-  task: DatabaseTask;
+  task: Task;
 };
 
 const TaskCard = ({ task }: TaskCardProps) => {
   return (
     <View style={{ backgroundColor: "#EAEAEA", padding: 16, borderRadius: 8 }}>
-      <Text>{task.title}</Text>
+      <Text style={{ fontSize: 16, fontWeight: 600 }}>{task.title}</Text>
       {task.description && <Text>{task.description}</Text>}
       <Text>{task.size}</Text>
+      {task.subTasks.length > 0 && (
+        <View style={{ padding: 8, gap: 8 }}>
+          {task.subTasks.map((subtask) => (
+            <View key={`${task.id}-${subtask.id}`}>
+              <Text>{subtask.title}</Text>
+              <Text>
+                {subtask.description && <Text>{subtask.description}</Text>}
+              </Text>
+            </View>
+          ))}
+        </View>
+      )}
     </View>
   );
 };
