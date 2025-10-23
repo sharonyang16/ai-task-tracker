dffaef1954db1a6689de004e261f787663884fe8
Sharon Yang <sharonyyang0@gmail.com>
2025-10-23 13:49:49 -0400

    remove old call

--- frontend/services/task-services.ts
+++ frontend/services/task-services.ts
@@ -3,14 +3,4 @@ import { DatabaseTask } from "@/types/tasks";
 
 const TASKS_API_URL = `${process.env.EXPO_PUBLIC_SERVER_URL}/tasks`;
 
-const getTasksByUUID = async (uuid: string): Promise<DatabaseTask[]> => {
-  const res = await api.get(`${TASKS_API_URL}/tasks/${uuid}`);
-
-  if (res.status !== 200) {
-    throw new Error("Error when fetching tasks");
-  }
-
-  return res.data;
-};
-
-export { getTasksByUUID };
+export {};
