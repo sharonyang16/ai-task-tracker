f60c704ffa510e28a83aa758cd333416b1100a89
Sharon Yang <92332953+sharonyang16@users.noreply.github.com>
2025-10-23 19:59:53 -0400

    Update Task Cards (#22)
    * add spacing in between
    
    * convert types
    
    * show subtasks
    
    * style card

--- frontend/components/home/task-card.tsx
+++ frontend/components/home/task-card.tsx
@@ -1,18 +1,50 @@
 import React from "react";
 import { View, Text } from "react-native";
-
-import { DatabaseTask } from "@/types/tasks";
+import { Checkbox } from "expo-checkbox";
+import { Task } from "@/types/tasks";
 
 type TaskCardProps = {
-  task: DatabaseTask;
+  task: Task;
 };
 
 const TaskCard = ({ task }: TaskCardProps) => {
   return (
     <View style={{ backgroundColor: "#EAEAEA", padding: 16, borderRadius: 8 }}>
-      <Text>{task.title}</Text>
-      {task.description && <Text>{task.description}</Text>}
-      <Text>{task.size}</Text>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: 16,
+          justifyContent: "space-between",
+        }}
+      >
+        <Checkbox value={task.isComplete} />
+        <View style={{ flex: 1 }}>
+          <Text style={{ fontSize: 16, fontWeight: 600 }}>{task.title}</Text>
+          {task.description && <Text>{task.description}</Text>}
+          <Text>{task.size}</Text>
+          {task.subTasks.length > 0 && (
+            <View style={{ padding: 8, gap: 8 }}>
+              {task.subTasks.map((subtask) => (
+                <View
+                  key={`${task.id}-${subtask.id}`}
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: 16,
+                  }}
+                >
+                  <Checkbox value={task.isComplete} />
+                  <View style={{ flex: 1 }}>
+                    <Text style={{fontWeight: 600}}>{subtask.title}</Text>
+                    {subtask.description && <Text>{subtask.description}</Text>}
+                  </View>
+                </View>
+              ))}
+            </View>
+          )}
+        </View>
+      </View>
     </View>
   );
 };
--- frontend/hooks/useHomePage.ts
+++ frontend/hooks/useHomePage.ts
@@ -1,18 +1,28 @@
 import { useEffect, useState } from "react";
-import { DatabaseTask } from "@/types/tasks";
+import { DatabaseTask, Task } from "@/types/tasks";
 import { useAuthContext } from "@/context/auth-context";
 import { getUserTasks } from "@/services/user-services";
 
 const useHomePage = () => {
   const { user } = useAuthContext();
-  const [tasks, setTasks] = useState<DatabaseTask[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   useEffect(() => {
     const fetchTasks = async () => {
       if (user !== null) {
         const res = await getUserTasks(user.id);
-
-        setTasks(res);
+        const processedData: Task[] = res.map((dbTask: DatabaseTask) => ({
+          ...dbTask,
+          createdAt: new Date(dbTask.created_at),
+          subTasks: dbTask.sub_tasks.map((subtask) => ({
+            ...subtask,
+            createdAt: new Date(subtask.created_at),
+          })),
+          createdBy: dbTask.created_by,
+          isComplete: dbTask.is_complete,
+        }));
+        
+        setTasks(processedData);
       }
     };
 
